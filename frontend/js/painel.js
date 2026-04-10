e916d749a8b20f1fdde14a1478ab5b077e850c3f
Pedro Teixeira <pharaujo915@gmail.com>
2026-04-09 23:06:23 -0300

    debug

--- frontend/js/painel.js
+++ frontend/js/painel.js
@@ -1,7 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
 	const token = localStorage.getItem("token");
 	const role = localStorage.getItem("role");
-
 	if (!token || role !== "admin") {
 		window.location.href = "index.html";
 		return;
